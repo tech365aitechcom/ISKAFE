a7bff9e14a38394a5bb7f7cfe65bee985b08bebf
ArijitPatra2906 <patraarijit440@gmail.com>
2025-03-27 20:30:00 +0530

    build fix

--- src/app/training-facilities/[id]/page.js
+++ src/app/training-facilities/[id]/page.js
@@ -7,12 +7,12 @@ const TrainingFacilitiesDetailsPage = () => {
       <div className='flex items-center gap-6'>
         <Image
           src='/sportskungfu.png'
-          alt=''
+          alt='Arnett Sport Kung Fu Association'
           width={140}
           height={140}
           className='object-contain h-44 w-44'
         />
-        <div className=''>
+        <div>
           <h2 className='text-3xl md:text-5xl mb-4 font-bold uppercase'>
             Arnett Sport Kung Fu Association
           </h2>
@@ -22,8 +22,8 @@ const TrainingFacilitiesDetailsPage = () => {
               Suite 122A
             </h2>
             <div className='flex items-center text-white'>
-              <img src='/Flag.png' alt='USA Flag' className='w-5 h-3 mr-2' />
-              <span className=''>Jacksonville, FL, USA</span>
+              <Image src='/Flag.png' alt='USA Flag' width={20} height={12} />
+              <span className='ml-2'>Jacksonville, FL, USA</span>
             </div>
           </div>
         </div>
@@ -33,9 +33,9 @@ const TrainingFacilitiesDetailsPage = () => {
           <span className='text-[#BDBDBD] text-xl'>Location:</span> 580 Ellis Rd
           S, Suite 122A
         </h2>
-        <div className='flex items-center text-white'>
-          <img src='/Flag.png' alt='USA Flag' className='w-5 h-3 mr-2' />
-          <span className='text-sm'>Jacksonville, FL, USA</span>
+        <div className='flex items-center gap-2 text-white'>
+          <Image src='/Flag.png' alt='USA Flag' width={20} height={12} />
+          <span className='text-sm ml-2'>Jacksonville, FL, USA</span>
         </div>
       </div>
       <p className='my-12 text-xl leading-7 font-medium hidden md:block'>
@@ -45,29 +45,26 @@ const TrainingFacilitiesDetailsPage = () => {
         comprehensive martial arts experience that blends discipline, technique,
         and practical application. Our expert instructors are committed to
         guiding you through every step of your martial arts journey, whether
-        you're a beginner looking to learn the basics or an advanced student
-        aiming to refine your skills.
+        you&apos;re a beginner looking to learn the basics or an advanced
+        student aiming to refine your skills.
       </p>
-      <div className='md:hidden my-6 '>
+      <div className='md:hidden my-6'>
         <h3 className='text-[#BDBDBD] text-xl'>About</h3>
         <p className='leading-7 font-medium'>
           February 2022 - Johnny Davis is a Former 2x World Kickboxing Champion
-          and now Global V.P. of Operations and Promotions for International
+          &amp; now Global V.P. of Operations and Promotions for International
           Kickboxing Federation (IKF)....IKF CEO Ms. Toni Fossum. He is also
           President: IKF Point Muay Thai (PMT), Point Kickboxing (PKB), and
-          Point Boxing Sparring Circuit (PBSC). Additionally, he's owner and
-          President of www.AKPLive.com a Pay Per View Streaming Company with
-          partner Mr. Derrick Rhems and has other business entities. Johnny
-          Morris Davis (born July 15, 1962) is an American former kickboxer who
-          competed in the welterweight and middleweight divisions. Nicknamed
-          "Superfoot",
+          Point Boxing Sparring Circuit (PBSC). Additionally, he&apos;s owner
+          and President of <a href='https://www.AKPLive.com'>www.AKPLive.com</a>
+          , a Pay Per View Streaming Company.
         </p>
       </div>
       <div className='bg-[#28133A80] p-4 rounded'>
         <h2 className='text-2xl md:text-4xl font-bold mb-4'>
           Trainers and Fighters
         </h2>
-        <div className='flex flex-col items-center justify-between gap-4'>
+        <div className='flex flex-col md:flex-row items-center justify-between gap-4'>
           <div className='relative'>
             <Image
               src='/fighter.png'
