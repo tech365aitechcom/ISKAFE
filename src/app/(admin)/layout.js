bea958b5cbdd2efd30fc343636aebed5ffebd5d2
ArijitPatra2906 <patraarijit440@gmail.com>
2025-04-15 09:51:05 +0530

    side bar

--- src/app/(admin)/layout.js
+++ src/app/(admin)/layout.js
@@ -6,9 +6,7 @@ const AdminLayout = ({ children }) => {
     <div className='flex h-screen inset-0 bg-gradient-to-br from-blue-50 to-indigo-100'>
       <Sidebar />
       <div className='flex-1 flex flex-col overflow-hidden'>
-        <main className='flex-1 overflow-y-auto p-4 ml-0 md:ml-64'>
-          {children}
-        </main>
+        <main className='flex-1 overflow-y-auto p-4'>{children}</main>
       </div>
     </div>
   )
