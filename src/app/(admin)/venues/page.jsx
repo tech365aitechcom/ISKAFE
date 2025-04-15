001818a931213c64331ace4d3b6831f3b99a014d
ArijitPatra2906 <patraarijit440@gmail.com>
2025-04-15 17:44:09 +0530

    Venue page

--- src/app/(admin)/venues/page.jsx
+++ src/app/(admin)/venues/page.jsx
@@ -1,7 +1,7 @@
-import { events } from '@/constants'
-import { EventTable } from './_components/EventTable'
+import { events, venues } from '@/constants'
+import { EventTable, VenuesTable } from './_components/EventTable'
 
-export default function EventsPage() {
+export default function VenuesPage() {
   return (
     <div className=' text-white p-8 flex justify-center relative overflow-hidden'>
       <div
@@ -13,7 +13,7 @@ export default function EventsPage() {
       ></div>
       <div className='bg-[#0B1739] bg-opacity-80 rounded-lg p-10 shadow-lg w-full z-50'>
         <div className='flex justify-between items-center mb-6'>
-          <h2 className='text-2xl font-semibold leading-8'>Events</h2>
+          <h2 className='text-2xl font-semibold leading-8'>Venues</h2>
           <button
             className='text-white px-4 py-2 rounded-md'
             style={{
@@ -24,7 +24,7 @@ export default function EventsPage() {
             Create New
           </button>
         </div>
-        <EventTable events={events} />
+        <VenuesTable venues={venues} />
       </div>
     </div>
   )
