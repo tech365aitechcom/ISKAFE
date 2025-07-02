0016ac42d38ca233b93c26a88b7b1ddd0adf74c6
ArijitPatra2906 <patraarijit440@gmail.com>
2025-07-02 13:25:52 +0530

    contact form fix

--- src/constants/index.js
+++ src/constants/index.js
@@ -2622,3 +2622,50 @@ export const countries = [
   'Zambia',
   'Zimbabwe',
 ]
+
+export const subtopicsByTopic = [
+  {
+    title: 'Events',
+    subtopics: [
+      'Registering to Compete',
+      'Payment Issue or Question',
+      'Matchmaking Question',
+      'Incorrect Bout Result',
+      'Other Event-Related Topic',
+    ],
+  },
+  {
+    title: 'Suspensions',
+    subtopics: [
+      'Reason for Suspension',
+      'Ask for Medical Clearance',
+      'Challenge a Suspension',
+    ],
+  },
+  {
+    title: 'Accounts',
+    subtopics: [
+      'Creating an Account',
+      'Editing an Account',
+      'Changing Your Email Address',
+      'Forgot Password',
+      'Updating Your Fight Record',
+    ],
+  },
+  {
+    title: 'General ISKA Questions',
+    subtopics: [],
+  },
+  {
+    title: 'Advertising & Sponsorships',
+    subtopics: [],
+  },
+  {
+    title: 'Using the Fight Platform for Your Event',
+    subtopics: [],
+  },
+  {
+    title: 'Other',
+    subtopics: [],
+  },
+]
